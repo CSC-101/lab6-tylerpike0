046d769dd672951add5bbf0b849a146818420934
TNTNINJA2 <108622789+TNTNINJA2@users.noreply.github.com>
2024-11-04 16:18:17 -0800

    Completed tasks 1 - 4

--- lab6.py
+++ lab6.py
@@ -37,12 +37,92 @@ def selection_sort(values:list[int]) -> None:
 
 
 # Part 1
+from data import Book
+
+# takes in a list of books and two bounding indexes, and returns the first alphabetical
+# number from that range
+# input: list[Book]
+# input: int
+# input: int
+# output: None
+def index_alphabetical_first_from(books:list[Book], start:int) -> Optional[int]:
+    if start >= len(books) or start < 0:
+        return None
+
+    first_index = start
+    for idx in range(start + 1, len(books)):
+        if books[idx].title < books[first_index].title:
+            first_index = idx
+    return first_index
+
+# takes in two books, and returns true if the first comes first alphabetically
+# input: Book
+# input: Book
+# output: bool
+def alphabetical_ascending(book1: Book, book2: Book) -> bool:
+    return book1.title.lower() < book2.title.upper()
+
+# takes in a list of books and sorts it alphabetically by their titles
+# input: list[Book]
+# output: None
+def selection_sort_books(books:list[Book]) -> None:
+    for index in range(len(books) - 1):
+        alphabetical_first_index = index_alphabetical_first_from(books, index)
+        tmp = books[alphabetical_first_index]
+        books[alphabetical_first_index] = books[index]
+        books[index] = tmp
 
 
 # Part 2
 
+# takes in a string and returns a string that has the characters, but letters have
+# swapped case
+# input: str
+# output: str
+def swap_case(input_string: str) -> str:
+    swapped_string = ""
+    for character in input_string:
+        if character.isalpha():
+            if character.islower():
+                swapped_string += character.upper()
+            else:
+                swapped_string += character.lower()
+        else:
+            swapped_string += character
+    return swapped_string
+
 
 # Part 3
 
+# takes in a string and two characters, and returns a string with all the character of
+# the second input, replaced with those of the third
+# input = str
+# input = str
+# input = str
+# output = str
+def str_translate(input_string: str, old: str, new:str) -> str:
+    translated_string = ""
+    for character in input_string:
+        if character == old:
+            translated_string += new
+        else:
+            translated_string += character
+
+    return translated_string
+
 
 # Part 4
+
+# takes in a string, and returns a dictionary where each key is a word, with the value being
+# the number of times that word appears in the str
+# input: str
+# output: dict[str: int]
+def histogram(input_string:str) -> dict[str: int]:
+    word_counts = {}
+    for word in input_string.split():
+        if word in word_counts.keys():
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+    return word_counts
